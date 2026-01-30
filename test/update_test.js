9e9b7cb52d20782abc8e3d09d68de097888966d3
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2026-01-30 19:46:05 +0530

    Operators in MongoDB

--- test/update_test.js
+++ test/update_test.js
@@ -6,11 +6,16 @@ describe("Updating records", () => {
   let eren2;
 
   beforeEach((done) => {
-    eren = new Student({ name: "Eren", studentNumber: 2520 });
-    eren2 = new Student({ name: "Eren", studentNumber: 2530 });
-    (eren2.save(), eren.save()).then(() => done());
-    console.log(eren);
-    console.log(eren2);
+    eren = new Student({
+      name: "Eren",
+      studentNumber: 2520,
+      articleCount: 4,
+      grade: 10,
+    });
+    // eren2 = new Student({ name: "Eren", studentNumber: 2530, grade: 85 });
+    eren.save().then(() => done());
+    // console.log(eren);
+    // console.log(eren2);
   });
 
   //   it("set and save", (done) => {
@@ -36,13 +41,24 @@ describe("Updating records", () => {
   //     console.log(res);
   //   });
 
-  it("Update one of the Erens", async () => {
-    const student = await Student.updateMany(
+  // it("Update one of the Erens", async () => {
+  //   const student = await Student.updateMany(
+  //     { name: "Eren" },
+  //     { studentNumber: 3000 },
+  //   );
+  //   const res = await Student.find({});
+  //   assert(res[0].studentNumber === 3000 && res[1].studentNumber === 3000);
+  //   console.log(res);
+  // });
+
+  it("Update grade", async () => {
+    const artCount = await Student.findOne({ name: "Eren" });
+    const student = await Student.updateOne(
       { name: "Eren" },
-      { studentNumber: 3000 },
+      { $mul: { grade: artCount.articleCount } },
     );
-    const res = await Student.find({});
-    assert(res[0].studentNumber === 3000 && res[1].studentNumber === 3000);
+    const res = await Student.find({ name: "Eren" });
+    assert(res[0].grade === 40);
     console.log(res);
   });
 });
